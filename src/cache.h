5d46f4842b025a2a81d4a1cc59a1282b9b0a37a8
David Banks <dave@hoglet.com>
2018-06-14 13:03:15 +0100

    Make all memory below 128MB L1/L2 cached

--- src/cache.h
+++ src/cache.h
@@ -3,10 +3,10 @@
 #ifndef CACHE_H
 #define CACHE_H
 
-// Memory below 64MB is L1 and L2 cached (inner and outer)
+// Memory below 128MB is L1 and L2 cached (inner and outer)
 
-// Mark the memory above 64MB to 128MB as L2 cached only (outer)
-#define L2_CACHED_MEM_BASE 0x04000000
+// Current no allocation for L2 cached only (outer)
+#define L2_CACHED_MEM_BASE 0x08000000
 
 // Mark the memory above 128MB as uncachable 
 #define UNCACHED_MEM_BASE 0x08000000
