eb6da66e9f5e13c2f84d37b048490966f556f447
David Banks <dave@hoglet.com>
2018-06-16 14:21:05 +0100

    Changes to work with 3m30s clip

--- src/cache.h
+++ src/cache.h
@@ -6,10 +6,10 @@
 // Memory below 128MB is L1 and L2 cached (inner and outer)
 
 // Current no allocation for L2 cached only (outer)
-#define L2_CACHED_MEM_BASE 0x08000000
+#define L2_CACHED_MEM_BASE 0x1F000000
 
 // Mark the memory above 128MB as uncachable 
-#define UNCACHED_MEM_BASE 0x08000000
+#define UNCACHED_MEM_BASE 0x1F000000
 
 // Location of the high vectors (last page of L1 cached memory)
 #define HIGH_VECTORS_BASE (L2_CACHED_MEM_BASE - 0x1000)
